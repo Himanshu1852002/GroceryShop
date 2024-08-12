c9bfcf013a6a26cd12c15c93249a7ff6835e97e5
Himanshu Vishwakarma <himanshu77jangid@gmail.com>
2024-08-12 11:25:01 +0530

    Add Offer Banner and some images

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero_Banner from './Components/Hero_Banner/Hero_Banner';
 import Explore_Menu from './Components/Explore_Menu/Explore_Menu';
 import Featured_Product from './Components/Featured_Product/Featured_Product';
+import Offer_Banner from './Components/Offer_Banner/Offer_Banner';
+
 function App() {
 
   return (
@@ -15,6 +17,7 @@ function App() {
       <Hero_Banner />
       <Explore_Menu />
       <Featured_Product />
+      <Offer_Banner />
     </>
   )
 }
---
+++ src/Components/Offer_Banner/Offer_Banner.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import './Offer_Banner.css';
+import tokri_img from '../Assets/Images/tokri.png';
+
+const Offer_Banner = () => {
+    return (
+
+        <div className="boxes">
+            <div className="container">
+                <div className="row d-flex">
+                    <div className="col-lg-6 col-md-6 col-sm-6 mt-5 mt-md-4 mt-sm-3 boxes1">
+                        <h2 className="ms-md-0 ms-sm-0">SAVE UP TO 20%</h2>
+                        <h2 className="ms-md-0 ms-sm-0">Organic Foods</h2>
+                        <p className="ms-md-0 ms-sm-0">Start your daily shopping with some Organic Foods.</p>
+                        <button className=" btn ms-md-0 ms-sm-0">Order Now</button>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-6 boxes2">
+                        <img src={tokri_img} alt />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Offer_Banner;
